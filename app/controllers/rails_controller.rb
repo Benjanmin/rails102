781180eb2943da64af8ff961ec616a3c7f818cd6
Benjanmin <893527442@qq.com>
2019-09-17 21:50:02 -0700

    implement my groups

---
+++ app/controllers/rails_controller.rb
@@ -0,0 +1,10 @@
+class RailsController < ApplicationController
+  def g
+  end
+
+  def controller
+  end
+
+  def account/groups
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,25 @@
 Rails.application.routes.draw do
+  get 'rails/g'
+
+  get 'rails/controller'
+
+  get 'rails/account/groups'
+
   devise_for :users
   resources :groups do
     member do
       post :join
       post :quit
     end
-  resources :posts
-end
-  root "groups#index"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+    resources :posts
+  end
+
+  namespace :account do
+    resources :groups
+ end
+
+ # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+ root "groups#index"
 end
